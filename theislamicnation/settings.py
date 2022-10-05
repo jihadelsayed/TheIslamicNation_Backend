9d36add518ce9fb6fae02959236cffbcb2bb788d
davidpales1 <david.pales1@gmail.com>
2022-10-05 22:56:59 +0200

    Update settings.py

--- theislamicnation/settings.py
+++ theislamicnation/settings.py
@@ -148,6 +148,8 @@ CORS_ORIGIN_WHITELIST = [
     'https://theislamicnation-2222.firebaseapp.com',
     'https://www.theislamicnation.com',
     'https://theislamicnation.com',
+    'https://account.neetechs.com',
+    'https://neetechs.com',
     'http://127.0.0.1:8000',
     'http://localhost:8000',
     'http://127.0.0.1:4200',
